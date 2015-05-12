aecbd29f1299df60bc665e74cfd30727bfe8d215
深尾 望 <nozomi@momo.local>
2015-05-12 14:59:21 +0900

    Initial Commit

---
+++ practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  practice
+//
+//  Created by 深尾 望 on 2015/05/12.
+//  Copyright (c) 2015年 深尾 望. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
